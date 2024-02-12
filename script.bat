e08112b2cc3a0eb4267a8b132c33ead43831028f
Daniel Beníček <66641432+lmaochroch@users.noreply.github.com>
2024-02-12 10:50:08 +0100

    updated script.bat

--- script.bat
+++ script.bat
@@ -1,18 +1,16 @@
 @echo off
 
 set fileUsername=
-set passwrd=123
 
 :parameters
 	if /I %~1 == -file SET fileUsername=%2& SHIFT
-	if /I %~1 == -pswd SET passwrd=%2& SHIFT
 	if /I %~1 == -help goto help shift
 	SHIFT
 
 if not (%1)==() goto parameters
 
 
-FOR /F "tokens=*" %%i IN (%fileUsername%) DO call :vytvoreni %%i %passwrd%
+FOR /F "tokens=*" %%i IN (%fileUsername%) DO call :vytvoreni %%i
 
 :vytvoreni
 net localgroup %4 /add
@@ -28,6 +26,5 @@ goto END
 :help
 echo help:
 echo -file --cesta k souboru s uzivatelskyma jmenama
-echo -pswd --heslo k uctu
 
 :END
